3426c4387bf520e701064f5e9ea05f28f0f3c478
jicheng.tang <tangjch15@gmail.com>
2025-01-27 07:39:41 +0000

    add fx logger

---
+++ fx1/demo4/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+	"io"
+	"net"
+	"net/http"
+)
+
+// EchoHandler is an http.Handler that copies its request body
+// back to the response.
+type EchoHandler struct {
+	log *zap.Logger
+}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewEchoHandler(log *zap.Logger) *EchoHandler {
+	return &EchoHandler{
+		log: log,
+	}
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		h.log.Warn("Failed to handle request", zap.Error(err))
+	}
+}
+
+// NewServeMux builds a ServeMux that will route requests
+// to the given EchoHandler.
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			log.Info("Starting HTTP server", zap.String("addr", srv.Addr))
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+func main() {
+	fx.New(
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+		fx.Provide(
+			NewHTTPServer,
+			NewServeMux,
+			NewEchoHandler,
+			zap.NewExample,
+		),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
+}
